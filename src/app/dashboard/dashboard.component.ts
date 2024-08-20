df29dfd6841f30d217c9e7223f983642a13ef956
harigovindvh <harigovindvh8@gmail.com>
2024-08-20 18:26:10 +0530

    initial skeleton commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { catchError, debounceTime, distinctUntilChanged, map, Observable, of, switchMap } from 'rxjs';
+import { Task } from './task.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  start: string = '';
+  end: string = '';
+  task: string = '';
+  logs: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get<any[]>('https://63d74fd85c4274b136f1fda5.mockapi.io/api/v1/log')
+      .subscribe((data) => {
+        this.logs = data;
+      });
+  }
+
+  onSubmit() {
+    const log = {
+      task: this.task,
+      start: this.start,
+      end: this.end,
+    };
+
+    this.http
+      .post('https://63d74fd85c4274b136f1fda5.mockapi.io/api/v1/log', log)
+      .subscribe((response) => {
+        this.logs.push(response);
+      });
+  }
+
+  search = (text$: Observable<string>) => text$.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+    switchMap(term =>term.length<2 ? of([]) : this.http.get<Task[]>(`https://63d74fd85c4274b136f1fda5.mockapi.io/api/v1/task?name=${term}`).pipe(
+      map(response => response.map(responseItem => responseItem.name) || []),
+      catchError(() => of([]))
+    )
+    ))
 }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule,
+    NgbTypeahead
   ]
 })
 export class DashboardModule { }
